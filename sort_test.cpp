dfe5cbd5a0f5b72fa1cdfa402099e869139d8a29
jeremy861030 <jeremy861030@gamil.com>
2018-07-26 14:46:02 +0800

    7/26

--- sort_test.cpp
+++ sort_test.cpp
@@ -4,12 +4,13 @@
 
 using namespace std;
 
-int a[5] = {3, 2, 5, 4, 1};
-int b[5] = {5, 4, 3, 2, 1};
-int c[5] = {-1, 3, -2, 4, 0};
-int d[5] = {1, 1, 1, 1, 1};
+int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
 
 int main(int argc, char **argv){
+ //    // 可以手動輸入數字進行排序檢測
 	// cout << "Input 5 numbers for sorting" << endl;
 	// for(int i = 0; i < 5; i++){
 	// 	cin >> a[i];
@@ -20,6 +21,7 @@ int main(int argc, char **argv){
     select_sort(c, 5);
     select_sort(d, 5);
 
+ //    // 輸出輸入的數字確認是否輸入有誤
 	// for(int i = 0; i < 5; i++){
 	// 	cout << a[i] << " " ;
 	// }
@@ -29,6 +31,7 @@ int main(int argc, char **argv){
     return RUN_ALL_TESTS();
 }
 
+// 檢測是否等於所預想的數字大小
 TEST(test_a, test){
     EXPECT_EQ(a[0], 1);
     EXPECT_EQ(a[1], 2);
@@ -58,7 +61,7 @@ TEST(test_d, test){
     EXPECT_EQ(d[4], 1);
 }
 
-
+// // 用小於等於後面數字的檢測方式
 // TEST(test_sort_2, test_1){
 // 	for(int i = 0; i < 4; i++){
 // 		EXPECT_LE(a[i], a[i+1]);
