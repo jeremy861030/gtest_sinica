8be3ae0f6907eb6c52221bf31c2790e5b65fe97e
jeremy861030 <jeremy861030@gamil.com>
2018-07-26 16:53:52 +0800

    7/26

--- sortMethods.c
+++ sortMethods.c
@@ -6,7 +6,6 @@
 //#include <windows.h>
 #include <stdio.h>
 #endif
-#include "sortMethods.h"
 
 /*
 ================================================
@@ -297,53 +296,3 @@ void heap_sort(int *x, int n)
 }
 
 
-#if 1
-int main()
-{ 
- #define MAX 5
- int *p, i, a[MAX];
- 
- /*录入测试数据*/
- p = a;
- printf("Input %d number for sorting :\n",MAX);
- for (i=0; i<MAX; i++)
- {
-  scanf("%d",p++);
- }
- printf("\n");
- /*测试选择排序*/
- 
- // p = a;
- // select_sort(p,MAX);
- /**/
-
- /*测试直接插入排序*/
- /*
- p = a;
- insert_sort(p,MAX);
- */
- 
- /*测试冒泡排序*/
- /*
- p = a;
- insert_sort(p,MAX);
- */
- /*测试快速排序*/
- 
- p = a;
- quick_sort(p,0,MAX-1);
- printf("%s\n", "quick");
- /*测试堆排序*/
- /*
- p = a;
- heap_sort(p,MAX);
- */
- for (p=a, i=0; i<MAX; i++)
- {
-  printf("%d ",*p++);
- }
- 
- printf("\n");
-// system("pause");
-}
-#endif
--- sort_test.cpp
+++ sort_test.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
 #include <gtest/gtest.h>
-#include "selectsort.h"
+#include "sortMethods.h"
 
 using namespace std;
 
-int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
-int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
-int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
-int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
 
 int main(int argc, char **argv){
  //    // 可以手動輸入數字進行排序檢測
@@ -15,11 +11,6 @@ int main(int argc, char **argv){
 	// for(int i = 0; i < 5; i++){
 	// 	cin >> a[i];
 	// }
-
-    select_sort(a, 5);
-    select_sort(b, 5);
-    select_sort(c, 5);
-    select_sort(d, 5);
 
  //    // 輸出輸入的數字確認是否輸入有誤
 	// for(int i = 0; i < 5; i++){
@@ -31,29 +22,45 @@ int main(int argc, char **argv){
     return RUN_ALL_TESTS();
 }
 
+// // 用小於等於後面數字的檢測方式
+// TEST(test_sort_2, test_1){
+// 	for(int i = 0; i < 4; i++){
+// 		EXPECT_LE(a[i], a[i+1]);
+// 	}
+// }
+
 // 檢測是否等於所預想的數字大小
-TEST(test_a, test){
+// select sort
+TEST(select_test, test_a){
+	int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+	select_sort(a, 5);
     EXPECT_EQ(a[0], 1);
     EXPECT_EQ(a[1], 2);
     EXPECT_EQ(a[2], 3);
     EXPECT_EQ(a[3], 4);
     EXPECT_EQ(a[4], 5);
 }
-TEST(test_b, test){
+TEST(select_test, test_b){
+	int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+	select_sort(b, 5);
     EXPECT_EQ(b[0], 1);
     EXPECT_EQ(b[1], 2);
     EXPECT_EQ(b[2], 3);
     EXPECT_EQ(b[3], 4);
     EXPECT_EQ(b[4], 5);
 }
-TEST(test_c, test){
+TEST(select_test, test_c){
+	int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+	select_sort(c, 5);
     EXPECT_EQ(c[0], -2);
     EXPECT_EQ(c[1], -1);
     EXPECT_EQ(c[2], 0);
     EXPECT_EQ(c[3], 3);
     EXPECT_EQ(c[4], 4);
 }
-TEST(test_d, test){
+TEST(select_test, test_d){
+	int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
+	select_sort(d, 5);
     EXPECT_EQ(d[0], 1);
     EXPECT_EQ(d[1], 1);
     EXPECT_EQ(d[2], 1);
@@ -61,9 +68,154 @@ TEST(test_d, test){
     EXPECT_EQ(d[4], 1);
 }
 
-// // 用小於等於後面數字的檢測方式
-// TEST(test_sort_2, test_1){
-// 	for(int i = 0; i < 4; i++){
-// 		EXPECT_LE(a[i], a[i+1]);
-// 	}
-// }
+// insert sort
+TEST(insert_test, test_a){
+	int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+	insert_sort(a, 5);
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+    EXPECT_EQ(a[2], 3);
+    EXPECT_EQ(a[3], 4);
+    EXPECT_EQ(a[4], 5);
+}
+TEST(insert_test, test_b){
+	int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+	insert_sort(b, 5);
+    EXPECT_EQ(b[0], 1);
+    EXPECT_EQ(b[1], 2);
+    EXPECT_EQ(b[2], 3);
+    EXPECT_EQ(b[3], 4);
+    EXPECT_EQ(b[4], 5);
+}
+TEST(insert_test, test_c){
+	int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+	insert_sort(c, 5);
+    EXPECT_EQ(c[0], -2);
+    EXPECT_EQ(c[1], -1);
+    EXPECT_EQ(c[2], 0);
+    EXPECT_EQ(c[3], 3);
+    EXPECT_EQ(c[4], 4);
+}
+TEST(insert_test, test_d){
+	int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
+	insert_sort(d, 5);
+    EXPECT_EQ(d[0], 1);
+    EXPECT_EQ(d[1], 1);
+    EXPECT_EQ(d[2], 1);
+    EXPECT_EQ(d[3], 1);
+    EXPECT_EQ(d[4], 1);
+}
+
+// bubble sort
+TEST(bubble_test, test_a){
+	int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+	bubble_sort(a, 5);
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+    EXPECT_EQ(a[2], 3);
+    EXPECT_EQ(a[3], 4);
+    EXPECT_EQ(a[4], 5);
+}
+TEST(bubble_test, test_b){
+	int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+	bubble_sort(b, 5);
+    EXPECT_EQ(b[0], 1);
+    EXPECT_EQ(b[1], 2);
+    EXPECT_EQ(b[2], 3);
+    EXPECT_EQ(b[3], 4);
+    EXPECT_EQ(b[4], 5);
+}
+TEST(bubble_test, test_c){
+	int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+	bubble_sort(c, 5);
+    EXPECT_EQ(c[0], -2);
+    EXPECT_EQ(c[1], -1);
+    EXPECT_EQ(c[2], 0);
+    EXPECT_EQ(c[3], 3);
+    EXPECT_EQ(c[4], 4);
+}
+TEST(bubble_test, test_d){
+	int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
+	bubble_sort(d, 5);
+    EXPECT_EQ(d[0], 1);
+    EXPECT_EQ(d[1], 1);
+    EXPECT_EQ(d[2], 1);
+    EXPECT_EQ(d[3], 1);
+    EXPECT_EQ(d[4], 1);
+}
+
+// shell sort
+TEST(shell_test, test_a){
+	int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+	shell_sort(a, 5);
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+    EXPECT_EQ(a[2], 3);
+    EXPECT_EQ(a[3], 4);
+    EXPECT_EQ(a[4], 5);
+}
+TEST(shell_test, test_b){
+	int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+	shell_sort(b, 5);
+    EXPECT_EQ(b[0], 1);
+    EXPECT_EQ(b[1], 2);
+    EXPECT_EQ(b[2], 3);
+    EXPECT_EQ(b[3], 4);
+    EXPECT_EQ(b[4], 5);
+}
+TEST(shell_test, test_c){
+	int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+	shell_sort(c, 5);
+    EXPECT_EQ(c[0], -2);
+    EXPECT_EQ(c[1], -1);
+    EXPECT_EQ(c[2], 0);
+    EXPECT_EQ(c[3], 3);
+    EXPECT_EQ(c[4], 4);
+}
+TEST(shell_test, test_d){
+	int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
+	shell_sort(d, 5);
+    EXPECT_EQ(d[0], 1);
+    EXPECT_EQ(d[1], 1);
+    EXPECT_EQ(d[2], 1);
+    EXPECT_EQ(d[3], 1);
+    EXPECT_EQ(d[4], 1);
+}
+
+// quick sort
+TEST(quick_test, test_a){
+	int a[5] = {3, 2, 5, 4, 1}; // 簡單的排序數字
+	quick_sort(a, 0, 4);
+    EXPECT_EQ(a[0], 1);
+    EXPECT_EQ(a[1], 2);
+    EXPECT_EQ(a[2], 3);
+    EXPECT_EQ(a[3], 4);
+    EXPECT_EQ(a[4], 5);
+}
+TEST(quick_test, test_b){
+	int b[5] = {5, 4, 3, 2, 1}; // 將整個排序整個反過來檢測
+	quick_sort(b, 0, 4);
+    EXPECT_EQ(b[0], 1);
+    EXPECT_EQ(b[1], 2);
+    EXPECT_EQ(b[2], 3);
+    EXPECT_EQ(b[3], 4);
+    EXPECT_EQ(b[4], 5);
+}
+TEST(quick_test, test_c){
+	int c[5] = {-1, 3, -2, 4, 0}; // 檢查可否排序負數
+	quick_sort(c, 0, 4);
+    EXPECT_EQ(c[0], -2);
+    EXPECT_EQ(c[1], -1);
+    EXPECT_EQ(c[2], 0);
+    EXPECT_EQ(c[3], 3);
+    EXPECT_EQ(c[4], 4);
+}
+TEST(quick_test, test_d){
+	int d[5] = {1, 1, 1, 1, 1}; // 排序一樣的數字
+	quick_sort(d, 0, 4);
+    EXPECT_EQ(d[0], 1);
+    EXPECT_EQ(d[1], 1);
+    EXPECT_EQ(d[2], 1);
+    EXPECT_EQ(d[3], 1);
+    EXPECT_EQ(d[4], 1);
+}
